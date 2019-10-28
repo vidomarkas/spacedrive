2bb3b6f068f5613e05ea18d90a97680169a4d436
Viktoras Domarkas <vidomarkas@gmail.com>
2019-10-28 18:44:13 +0000

    hide nav menu when clicked on link, add padding-top to header

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const hamburger = document.querySelector(".hamburger");
 const navMobile = document.querySelector(".navigation__mobile");
 const nav = document.querySelector("#navigation");
 const navList = document.querySelector("#navigation__list");
+const navLinks = document.querySelectorAll(".navigation__mobile__link");
+
+Array.from(navLinks).map(link => {
+  link.addEventListener("click", () => {
+    navMobile.style.display = "none";
+  });
+});
 
 hamburger.addEventListener("click", () => {
-  if (navMobile.style.display === "none") {
-    navMobile.style.display = "block";
-  } else {
+  if (navMobile.style.display === "block") {
     navMobile.style.display = "none";
+  } else {
+    navMobile.style.display = "block";
   }
 });
